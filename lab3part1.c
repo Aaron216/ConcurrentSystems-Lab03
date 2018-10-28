5063a2c649f766139454b8db262dfcd2a02a5ee2
Aaron Musgrave <aaron@winthropdc.com>
2018-10-28 12:45:36 +0800

    Completed part 1

--- lab3part1.c
+++ lab3part1.c
@@ -16,6 +16,7 @@
 #include <stdlib.h>
 #include <omp.h>
 #include "my_rand.h"
+#include "timer.h"
 
 /*---------------------------------------------------------------------*/
 int main(int argc, char* argv[]) {
@@ -23,6 +24,7 @@ int main(int argc, char* argv[]) {
     int thread_count;
     long long int number_in_circle;
     long long int number_of_tosses;
+    double start, finish;
 
     // Check correct number of arguments
     if (argc != 3) {
@@ -34,22 +36,30 @@ int main(int argc, char* argv[]) {
     thread_count = strtol(argv[1], NULL, 10);
     number_of_tosses = strtoll(argv[2], NULL, 10);
 
-    number_in_circle = 0; {
+    number_in_circle = 0;
+    GET_TIME(start)
+    #pragma omp parallel num_threads(thread_count) reduction(+: number_in_circle)
+    {
         int my_rank = omp_get_thread_num();
         unsigned seed = my_rank + 1;
         long long int toss;
         double x, y, distance_squared;
 
+        #pragma omp for
         for(toss = 0; toss < number_of_tosses; toss++) {
             x = 2*my_drand(&seed) - 1;
             y = 2*my_drand(&seed) - 1;
             distance_squared = x*x + y*y;
-            if (distance_squared <= 1) number_in_circle++;
+            if (distance_squared <= 1) {
+                number_in_circle++;
+            }
         }
     }
+    GET_TIME(finish);
 
     pi_estimate = 4*number_in_circle/((double) number_of_tosses);
     printf("Estimated pi: %e\n", pi_estimate);
+    printf("Time: %e\n", finish-start);
 
-    return 0;
+    return EXIT_SUCCESS;
 }
